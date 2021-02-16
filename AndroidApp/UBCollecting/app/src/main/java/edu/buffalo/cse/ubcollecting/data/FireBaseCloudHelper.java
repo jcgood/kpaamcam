f25b268e616891b354b29350a7c6687064e1dec0
Blake Cooper <blakecoo@buffalo.edu>
2021-02-16 13:06:00 -0500

    Added generalized methods to FireCloudHelper

--- AndroidApp/UBCollecting/app/src/main/java/edu/buffalo/cse/ubcollecting/data/FireBaseCloudHelper.java
+++ AndroidApp/UBCollecting/app/src/main/java/edu/buffalo/cse/ubcollecting/data/FireBaseCloudHelper.java
@@ -9,16 +9,20 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
 import edu.buffalo.cse.ubcollecting.EntryActivity;
 import edu.buffalo.cse.ubcollecting.app.App;
+import edu.buffalo.cse.ubcollecting.data.models.Model;
 import edu.buffalo.cse.ubcollecting.data.models.Person;
 import edu.buffalo.cse.ubcollecting.data.tables.Table;
 //THIS FILE IS AN ATTEMPT TO CENTRALIE CLOUD OPERATIONS
 //NOTE:NOT SURE IF THIS IS THE BEST METHOD YET
-public class FireBaseCloudHelper extends Application{
+public class FireBaseCloudHelper<E extends Model> extends Application{
 
     public FirebaseDatabase database;
     public DatabaseReference mDatabase;
@@ -26,6 +30,8 @@ public class FireBaseCloudHelper extends Application{
     private Context context;
     private String TAG;
     public boolean isConnected;
+    static final short GET = 3;
+
 
     public FireBaseCloudHelper(Context context) {
         this.context = context;
@@ -81,5 +87,30 @@ public class FireBaseCloudHelper extends Application{
         mDatabase.child("People").child(person.id).child("Email").setValue(email);
         mDatabase.child("People").child(person.id).child("password").setValue(password);
 
-    }
+    };
+
+    //The methods below were written by Blake to generalize the insert/delete/update process. They still need to be tested!
+    public void insert (Table<E> table, E entry) throws InvocationTargetException, IllegalAccessException {
+        String name = table.getTableName();
+        List<Method> entryMethods = entry.getGetters();
+
+        for (Method method: entryMethods) {
+            String key = method.getName().substring(GET, method.getName().length());
+            String value = (String) method.invoke(null);
+
+            mDatabase.child(name).child(entry.getIdentifier()).child(key).setValue(value);
+        };
+    };
+
+    public void delete (Table<E> table, String entryID) {
+        String name = table.getTableName();
+        mDatabase.child(name).child(entryID).removeValue();
+    };
+
+    //NOTE: this is a lazy implementation, it just deletes and re-inserts an entry. It should probably iterate over the values in the existing entry and only replace those values that have changed.
+    // I'm not sure the app even uses updates?
+    public void update (Table<E> table, E entry) throws InvocationTargetException, IllegalAccessException {
+        this.delete(table, entry.getIdentifier());
+        this.insert(table,entry);
+    };
 }
